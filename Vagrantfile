53b2907a468775ca0ba27fee0198a05b64d6ea6f
Heath Brown <heathd.brown@gmail.com>
2013-08-10 18:03:36 -0500

    Updating for dual machine run puppetmaster and puppet bootstrap scripts for puppetmaster, puppet

--- Vagrantfile
+++ Vagrantfile
@@ -6,4 +6,16 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise64"
+
+  config.vm.define :puppetmaster do |puppetmaster|
+      puppetmaster.vm.box = "precise64"
+      puppetmaster.vm.network :private_network, ip: "192.168.50.4"
+      puppetmaster.vm.provision :shell, :path => "install_puppetmaster.sh"
+  end
+
+  config.vm.define :puppet do |puppet|
+      puppet.vm.box = "precise64"
+      puppet.vm.network :private_network, ip: "192.168.50.5"
+      puppet.vm.provision :shell, :path => "install_puppet.sh"
+  end
 end
---
+++ install_puppetmaster.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#License
+#The use and distribution terms for this software are covered by the Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html at the root of this distribution. By using this software in any fashion, you are agreeing to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
+#Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+# used from https://github.com/niclasmeier/puppet-ec2-enc
+set -e -x
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Adding Puppetlabs repository..."
+curl -o /tmp/puppetlabs-release-precise.deb http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+dpkg -i /tmp/puppetlabs-release-precise.deb
+
+echo "Updating APT repositories..."
+apt-get update && apt-get upgrade -y
+
+echo "Installing puppet and facter..."
+sudo apt-get install puppetmaster facter -y
+
+echo "Wrinting puppet conf..."
+(cat <<"END"
+[main]
+logdir=/var/log/puppet
+vardir=/var/lib/puppet
+ssldir=/var/lib/puppet/ssl
+rundir=/var/run/puppet
+factpath=$vardir/lib/facter
+templatedir=$confdir/templates
+server=puppetmaster.example.com
+END
+) > /etc/puppet/puppet.conf
+
+echo "Enabling autostart for Puppet..."
+cat /etc/default/puppet | sed s/START=no/START=yes/g > /etc/default/puppet.tmp && mv /etc/default/puppet.tmp /etc/default/puppet
+
+echo "Stating Puppet ..."
+service puppetmaster start
